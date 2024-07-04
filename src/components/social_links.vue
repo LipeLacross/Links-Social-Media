116893a40ebbbfe537578f16aa44e2fac1c98ec5
Felipe Rios <feliperioscross.br@gmail.com>
2024-07-04 05:06:11 -0300

    Ícones, animação na foto

--- src/components/social_links.vue
+++ src/components/social_links.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="social-links">
     <a href="https://www.instagram.com/lipelacross/" class="social-link instagram" title="Instagram">
+      <img src="/src/assets/instagram.png" alt="Instagram" class="social-icon" />
       <span>Instagram</span>
     </a>
     <a href="https://www.linkedin.com/in/lipelacross-developer/" class="social-link linkedin" title="LinkedIn">
+      <img src="/src/assets/linkedin.png" alt="LinkedIn" class="social-icon" />
       <span>LinkedIn</span>
     </a>
     <a href="https://www.youtube.com/@DevLipeLacross" class="social-link youtube" title="YouTube">
+      <img src="/src/assets/youtube.png" alt="YouTube" class="social-icon" />
       <span>YouTube</span>
     </a>
-    <a href="https://github.com/LipeLacross" class="social-link github" title="GitHub">
+    <a href="https://github.com/seu-usuario-do-github" class="social-link github" title="GitHub">
+      <img src="/src/assets/github.png" alt="GitHub" class="social-icon" />
       <span>GitHub</span>
     </a>
     <div class="social-link portfolio" title="Em breve">
+      <img src="/src/assets/curriculum.png" alt="GitHub" class="social-icon" />
       <span>Em breve</span>
     </div>
   </div>
@@ -54,6 +59,11 @@
   cursor: pointer;
   transition: transform 0.3s ease, background-color 0.3s ease;
   animation: buttonHover 3s infinite;
+}
+.social-icon {
+  width: 30px; /* Ajuste conforme necessário */
+  height: auto;
+  margin-right: 10px; /* Espaço entre o ícone e o texto */
 }
 
 .social-link:hover {
